359f05f848ff9d9f47d1729f4c491f19535359bc
Mostafa Asaad <ma4190243@gmail.com>
2024-10-25 19:09:29 +0300

    Added the database connection configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: joi.object({
+        DB_DATABASE: joi.string().required(),
+        DB_USER: joi.string().required(),
+        DB_PASSWORD: joi.string().required(),
+        DB_PORT: joi.number().default('3306'),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (envConfigService: ConfigService) => ({
+        type: 'mysql',
+        entities: [],
+        database: envConfigService.get<string>('DB_DATABASE'),
+        username: envConfigService.get<string>('DB_USER'),
+        password: envConfigService.get<string>('DB_PASSWORD'),
+        port: envConfigService.get<number>('DB_PORT'),
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
