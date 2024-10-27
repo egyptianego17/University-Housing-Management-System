88dc59f161896a39eca08896e0292b91a31a45fc
Amr Yasser <amr.yassr.2@gmail.com>
2024-10-27 23:48:21 +0300

    Optimized imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -14,17 +14,6 @@ import { ManagedFloorsModule } from './modules/managed-floors/managed-floors.mod
 import { ManagedMealsModule } from './modules/managed-meals/managed-meals.module';
 import { NotificationModule } from './modules/notification/notification.module';
 import { StudentModule } from './modules/student/student.module';
-import { Announcement } from './modules/announcements/entities/announcement.entity';
-import { Attendance } from './modules/attendance/entities/attendance.entity';
-import { AttendanceManager } from './modules/attendance-manager/entities/attendance-manager.entity';
-import { CateringManager } from './modules/catering-manager/entities/catering-manager.entity';
-import { Complaint } from './modules/complaints/entities/complaint.entity';
-import { FloorManager } from './modules/floor-manager/entities/floor-manager.entity';
-import { ManagedFloor } from './modules/managed-floors/entities/managed-floor.entity';
-import { ManagedMeal } from './modules/managed-meals/entities/managed-meal.entity';
-import { Notification } from './modules/notification/entities/notification.entity';
-import { Student } from './modules/student/entities/student.entity';
-import { User } from './modules/user/entities/user.entity';
 import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
