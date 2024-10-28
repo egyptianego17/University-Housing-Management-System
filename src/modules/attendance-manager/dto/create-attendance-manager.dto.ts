1abfb213efad2aafc846a09c98c4b8bb12192567
Mostafa Asaad <ma4190243@gmail.com>
2024-10-28 22:15:47 +0300

    feat: Added attendance manager CRUD operations & swagger documentaation

--- src/modules/attendance-manager/dto/create-attendance-manager.dto.ts
+++ src/modules/attendance-manager/dto/create-attendance-manager.dto.ts
@@ -1,1 +1,8 @@
-export class CreateAttendanceManagerDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber } from 'class-validator';
+
+export class CreateAttendanceManagerDto {
+  @IsNumber()
+  @ApiProperty({ description: 'The user id for the attendance manager' })
+  userId: number;
+}
--- src/modules/attendance-manager/entities/attendance-manager.entity.ts
+++ src/modules/attendance-manager/entities/attendance-manager.entity.ts
@@ -1,7 +1,19 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+// attendance-manager.entity.ts
+import { Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { User } from 'src/modules/user/entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class AttendanceManager {
   @PrimaryGeneratedColumn('increment', { type: 'bigint' })
+  id!: number;
+
+  @ManyToOne(() => User, (user) => user.id, {
+    nullable: false,
+    onDelete: 'CASCADE',
+  })
   userId!: number;
+
+  @JoinColumn({ name: 'userId' })
+  user!: User;
 }
